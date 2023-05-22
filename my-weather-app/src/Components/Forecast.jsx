39ff72c2f86455f37709df5a071afbeb2ccf5100
loarden <biacsi.ors@gmail.com>
2023-05-22 16:53:13 +0200

    fixed forecast format

--- my-weather-app/src/Components/Forecast.jsx
+++ my-weather-app/src/Components/Forecast.jsx
@@ -1,4 +1,5 @@
 import { kelvinToCelsius } from "../Modules/unitExchange";
+import { formatDate } from "../Modules/format";
 
 function Forecast(props) {
   const time = props.time
@@ -7,7 +8,7 @@ function Forecast(props) {
 
   return (
     <div className="text-white flex justify-between items-center px-4 my-2 backdrop-blur-sm shadow bg-emerald-300/25">
-      <p>{time}</p>
+      <p className="font-bold text-lg">{formatDate(time)}</p>
       <div className="flex items-center gap-1">
         <h4 className="text-lg sm:text-xl font-bold">{kelvinToCelsius(temp, 1)}</h4>
         <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="icon" />
