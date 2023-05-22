77097a6987ec00982e66a88d354286a9ad638f9e
loarden <biacsi.ors@gmail.com>
2023-05-22 18:54:57 +0200

    clear code

--- my-weather-app/src/Components/NotFound.jsx
+++ my-weather-app/src/Components/NotFound.jsx
@@ -1,7 +1,6 @@
 function NotFound() {
   return (
     <div className=" w-full h-96 flex justify-center items-center">
-      {/* <img src="404.png"/> */}
       <h1 className="text-5xl text-white">City Not found</h1>
     </div>
   )
